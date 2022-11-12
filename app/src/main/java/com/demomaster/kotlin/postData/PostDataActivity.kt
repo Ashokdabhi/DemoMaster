4276a9e411b3ddfb36c2e4e3f4e5c8d67e78c03a
Ashok <dalvadiashok@gmail.com>
2022-11-12 15:24:25 +0530

    post data set

--- app/src/main/java/com/demomaster/kotlin/postData/PostDataActivity.kt
+++ app/src/main/java/com/demomaster/kotlin/postData/PostDataActivity.kt
@@ -7,7 +7,6 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.demomaster.R
 import com.demomaster.databinding.ActivityPostDataBinding
-import com.demomaster.demo_master.apiRecyleview.pojo.User
 import okhttp3.ResponseBody
 import org.json.JSONObject
 import retrofit2.Call
@@ -22,7 +21,7 @@ class PostDataActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityPostDataBinding.inflate(layoutInflater)
+        binding = ActivityPostDataBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         mContext = this@PostDataActivity
@@ -39,46 +38,54 @@ class PostDataActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View?) {
 
-        if (v!!.id==binding.btnSubmit.id){
+        if (v!!.id == binding.btnSubmit.id) {
             setValidation()
+            binding.layoutOutPut.visibility = View.GONE
         }
     }
 
     private fun setValidation() {
 
-        var isValidate=true
+        var isValidate = true
 
         val name = binding.tilName.editText!!.text.toString().trim()
         val job = binding.tilJob.editText!!.text.toString().trim()
 
-        if (name.isEmpty()){
-            isValidate=false
-            binding.tilName.editText!!.error="Enter Name"
+        if (name.isEmpty()) {
+            isValidate = false
+            binding.tilName.editText!!.error = "Enter Name"
         }
 
-        if (job.isEmpty()){
-            isValidate=false
-            binding.tilJob.editText!!.error="Enter Job"
+        if (job.isEmpty()) {
+            isValidate = false
+            binding.tilJob.editText!!.error = "Enter Job"
         }
-        if (isValidate){
-            binding.progressBar.visibility=View.VISIBLE
+        if (isValidate) {
+            binding.progressBar.visibility = View.VISIBLE
             restApiRequestsService = ApiClient.buildService(RestApiRequests::class.java)
 
             val call: Call<ResponseBody> =
-                restApiRequestsService!!.createUser(name,job)
+                restApiRequestsService!!.createUser(name, job)
 
             call.enqueue(object : Callback<ResponseBody> {
-                override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
-                    binding.progressBar.visibility=View.GONE
-                    if (response.code()==201){
+                override fun onResponse(
+                    call: Call<ResponseBody>,
+                    response: Response<ResponseBody>
+                ) {
+                    binding.progressBar.visibility = View.GONE
+                    if (response.code() == 201) {
 
                         val jsonObject = JSONObject(response.body()!!.string())
 
-                        val a =jsonObject.getString("id")
-                        val b =jsonObject.getString("createdAt")
+                        val id = jsonObject.getString("id")
+                        val date = jsonObject.getString("createdAt")
 
-                        Toast.makeText(mContext, "" + name+"\n"+job+"\n"+a+"\n"+b, Toast.LENGTH_LONG).show()
-                    }else{
+                        binding.layoutOutPut.visibility = View.VISIBLE
+                        binding.txtName.text = name
+                        binding.txtJob.text = job
+                        binding.txtId.text = id
+                        binding.txtDate.text = date
+                    } else {
                         Toast.makeText(mContext, "" + response.code(), Toast.LENGTH_SHORT).show()
                     }
                 }
@@ -87,7 +94,7 @@ class PostDataActivity : AppCompatActivity(), View.OnClickListener {
 
                     Toast.makeText(mContext, "" + t.message, Toast.LENGTH_SHORT).show()
                     call.cancel()
-                    binding.progressBar.visibility=View.GONE
+                    binding.progressBar.visibility = View.GONE
                 }
             })
         }
